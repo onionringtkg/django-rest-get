13828d08f0f3ef2e02aeacfdcd6b830cef593695
Okano Takumi <oknokn.of.12@gmail.com>
2020-10-12 17:06:43 +0900

    finish making front

--- src/components/DrfApiFetch.js
+++ src/components/DrfApiFetch.js
@@ -6,6 +6,7 @@ const DrfApiFetch = () => {
     const [tasks, setTasks] = useState([])
     const [selectedTask, setSelectedTask] = useState([])
     const [id, setId] = useState(1)
+    const [editedTask, setEditedTask] = useState({id:'', title:''})
 
     useEffect(() => {
         axios.get('http://127.0.0.1:8000/api/tasks', {
@@ -38,6 +39,41 @@ const DrfApiFetch = () => {
             )
     }
 
+    const createTask = (task) => {
+        const data = {
+            title: task.title
+        }
+        axios.post(`http://127.0.0.1:8000/api/tasks/`, data, {
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': 'Token 1a79345b2de5ee35ad2a2f82e87f42d7f42b9b84'
+            }
+        })
+        .then(
+            res => (setTasks([...tasks, res.data]))
+            )
+    }
+
+    const updateTask = (task) => {
+        axios.put(`http://127.0.0.1:8000/api/tasks/${task.id}`, task, {
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': 'Token 1a79345b2de5ee35ad2a2f82e87f42d7f42b9b84'
+            }
+        })
+        .then(
+            res => {setTasks(tasks.map(task => (task.id === editedTask.id ? res.data : task)));
+                    setEditedTask({id:'', title:''})
+                    }
+            )
+    }
+
+    const handleInputChange = () => evt => {
+        const value = evt.target.value;
+        const name = evt.target.name;
+        setEditedTask({...editedTask, [name]:value})
+    }
+
     return (
         <div>
             <ul>
@@ -45,6 +81,9 @@ const DrfApiFetch = () => {
                 tasks.map(task => <li key={task.id}> {task.title}  {task.id}
                 <button type='button' onClick={()=>deleteTask(task.id)}>
                     <i className="fas fa-trash-alt"></i>
+                </button>
+                <button type='button' onClick={()=>setEditedTask(task)}>
+                    <i className="fas fa-pen"></i>
                 </button>
                 </li>)
                 
@@ -58,6 +97,15 @@ const DrfApiFetch = () => {
             
 
             <h3>{selectedTask.title} {selectedTask.id}</h3>
+
+            <input type="text" name="title"
+            value = {editedTask.title}
+            onChange={handleInputChange()}
+            placeholder="New Task ?" required/>
+            { editedTask.id ?  
+            <button onClick={()=>updateTask(editedTask)}>Update</button>:
+            <button onClick={()=>createTask(editedTask)}>Create</button>
+            }
         </div>
     )
 }
